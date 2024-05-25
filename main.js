e46cb221a54c058a0a2591b8962d0f44d4ce4c59
goldipl <mgodlewski92@gmail.com>
2024-05-25 20:32:20 +0200

    change cube to ground and render it

--- main.js
+++ main.js
@@ -8,18 +8,18 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+/* ground */
+const ground = new THREE.Mesh(
+	new THREE.BoxGeometry( 30, 1, 30 ),
+	new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
+	);
+	ground.position.y = -1;
+scene.add( ground );
 
 camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 }
