19139195719a625f8c3ca1277cbe9895bfd7cc94
goldipl <mgodlewski92@gmail.com>
2024-05-25 22:50:02 +0200

    change camera position

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 /* camera position */
-camera.position.z = 5;
-camera.position.y = 0.9;
+camera.position.z = 3;
+camera.position.y = 1.1;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
