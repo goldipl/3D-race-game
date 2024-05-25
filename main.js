a607de1ea9b19833244f1f4b59a6ae244a184481
goldipl <mgodlewski92@gmail.com>
2024-05-25 20:50:24 +0200

    add player red car/object to the scene

--- main.js
+++ main.js
@@ -1,33 +1,41 @@
-import './style.css'
+import './style.css';
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+/* camera position */
+camera.position.z = 5;
+camera.position.y = 0.9;
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 /* ground */
 const ground = new THREE.Mesh(
-	new THREE.BoxGeometry( 30, 1, 30 ),
-	new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
-	);
-	ground.position.y = -1;
-scene.add( ground );
+  new THREE.BoxGeometry(30, 1, 30),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+ground.position.y = -1;
+scene.add(ground);
 
-camera.position.z = 5;
+/* player red car */
+const player = new THREE.Mesh(
+  new THREE.BoxGeometry(0.5, 0.5, 0.5),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+scene.add(player);
 
 function animate() {
-	requestAnimationFrame( animate );
-
-	renderer.render( scene, camera );
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
 
 animate();
 
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-})
+});
