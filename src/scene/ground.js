c5eef9a92549b6a3420076dea1d5b72f36380c92
goldipl <mgodlewski92@gmail.com>
2024-05-26 14:24:14 +0200

    add grass texture & change ground

--- src/scene/ground.js
+++ src/scene/ground.js
@@ -1,7 +1,21 @@
 import * as THREE from 'three';
 
+const textureLoader = new THREE.TextureLoader();
+const grassTexture = textureLoader.load('./src/img/grass.jpg');
+
+// Create the material with the loaded texture and darken it by setting a dark color
+const grassMaterial = new THREE.MeshBasicMaterial({
+    map: grassTexture,
+    color: new THREE.Color(0x929f2f),
+});
+
 export const ground = new THREE.Mesh(
     new THREE.BoxGeometry(30, 1, 30),
-    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    grassMaterial
 );
+
 ground.position.y = -1;
+
+grassTexture.wrapS = THREE.RepeatWrapping;
+grassTexture.wrapT = THREE.RepeatWrapping;
+grassTexture.repeat.set(20, 20);
