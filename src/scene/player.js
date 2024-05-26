002675cc0571326c33ca26409c8948c337aab6d5
goldipl <mgodlewski92@gmail.com>
2024-05-26 09:06:12 +0200

    change player length to 0.4

--- src/scene/player.js
+++ src/scene/player.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
 export const player = new THREE.Mesh(
-    new THREE.BoxGeometry(0.3, 0.2, 0.7),
+    new THREE.BoxGeometry(0.3, 0.2, 0.4),
     new THREE.MeshBasicMaterial({ color: 0xff0000 })
 );
