97cdb7b4bfe1e90fb32389361545413231f092e8
goldipl <mgodlewski92@gmail.com>
2024-05-26 14:23:59 +0200

    add blue sky

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ import { powerup } from './src/utils/powerup';
 import { cameraSettings } from './src/settings/settings';
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x00CCFF);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // Camera position
